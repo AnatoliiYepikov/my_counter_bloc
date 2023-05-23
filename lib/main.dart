cd96b8ffea3cb58b1542e90fa41e2d2c888b515e
AnatoliiYepikov <anatolii.yepikov.30.11.2022@gmail.com>
2023-05-23 11:29:58 +0200

    add UserBloc list 11:33

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_counter_bloc/counter_bloc.dart';
+import 'package:my_counter_bloc/user_bloc/bloc/user_bloc.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -22,35 +23,67 @@ class MyPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    CounterBloc bloc = CounterBloc();
-    return BlocProvider<CounterBloc>(
-      create: (context) => bloc,
+    CounterBloc counterBloc = CounterBloc();
+    UserBloc userBloc = UserBloc();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<CounterBloc>(
+          create: (context) => counterBloc,
+        ),
+        BlocProvider<UserBloc>(
+          create: (context) => userBloc,
+        ),
+      ],
       child: Scaffold(
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             IconButton(
                 onPressed: () {
-                  bloc.add(CounterIncrementEvent());
+                  counterBloc.add(CounterIncrementEvent());
                 },
                 icon: const Icon(Icons.plus_one)),
             IconButton(
                 onPressed: () {
-                  bloc.add(CounterDecrementEvent());
+                  counterBloc.add(CounterDecrementEvent());
                 },
                 icon: const Icon(Icons.exposure_minus_1)),
+            IconButton(
+                onPressed: () {
+                  userBloc.add(UserGetUsersEvent(counterBloc.state));
+                },
+                icon: const Icon(Icons.person)),
           ],
         ),
-        body: Center(
-            child: BlocBuilder<CounterBloc, int>(
-          bloc: bloc,
-          builder: (context, state) {
-            return Text(
-              state.toString(),
-              style: const TextStyle(fontSize: 77),
-            );
-          },
-        )),
+        body: SafeArea(
+          child: Center(
+              child: Column(
+            children: [
+              BlocBuilder<CounterBloc, int>(
+                bloc: counterBloc,
+                builder: (context, state) {
+                  return Text(
+                    state.toString(),
+                    style: const TextStyle(fontSize: 77),
+                  );
+                },
+              ),
+              BlocBuilder<UserBloc, UserState>(
+                bloc: userBloc,
+                builder: (context, state) {
+                  return Column(
+                    children: [
+                      if (state is UserLoadingState)
+                        const CircularProgressIndicator(),
+                      if (state is UserLoadedState)
+                        ...state.users.map((e) => Text(e.name)),
+                    ],
+                  );
+                },
+              ),
+            ],
+          )),
+        ),
       ),
     );
   }
